c98fdb85dcf326054e70adb7a2a34bd3a0d097d8
Benedikt Holland <benedikt.holland@protonmail.com>
2024-07-28 18:38:22 +0200

    🚸 use exercise weight directly as input instead of 'x'

--- fitness_tracker.py
+++ fitness_tracker.py
@@ -1,33 +1,26 @@
 # Check a .md file in your obsidian vault and construct a time series
 import pandas as pd
 from os.path import exists
+import os
+from functions import read_md, to_md
 
-INPUT = '../../obsidian/living/fitness/Gym.md'
-OUTPUT = 'history.csv'
 
-if __name__ == '__main__':
-    with open(INPUT, 'r') as f:
-        lines = f.readlines()
-    data = [] 
-    for line in lines:
-        if line[0] != "|":
-            continue  
-        # Data is kept in a markdown table
-        data.append([l.strip() for l in line[1:-2].split("|")])
-    header = data[0]
-    data = data[2:]  
-    df = pd.DataFrame(data, columns=header)
-    if exists(OUTPUT):
-        history = pd.read_csv("history.csv",index_col=0)
+if __name__ == "__main__":
+    input = os.getenv("DASHBOARD")
+    output = os.getenv("HISTORY")
+    df = read_md(input)
+    if exists(output):
+        history = pd.read_csv("history.csv", index_col=0)
     else:
         history = pd.DataFrame()
-    # Lines will be written to history if a column named 'check' contains an 'x'
-    done = df[df.check.str.contains('x')] 
-    done.check = pd.Timestamp.today().date()
-    done.set_index("check", inplace=True)
+    # Lines will be written to history if a column named 'value' contains a value
+    df["max"] = df[["last", "max", "value"]].max(axis=1)
+    done = df[df.value != ""].copy(deep=True)
+    done["date"] = pd.Timestamp.today().date()
+    done.set_index("date", inplace=True)
     history = pd.concat([history, done])
-    history.to_csv(OUTPUT)
-    # Remove checkmarks
-    with open(INPUT, 'w') as f:
-        for line in lines:
-            f.write(line.replace(' x ', '')) 
+    history.to_csv(output)
+    # Reset value
+    df.loc[df.value != "", "last"] = df.loc[df.value != "", "value"]
+    df.value = ""
+    to_md(df, os.getenv("DASHBOARD"))
