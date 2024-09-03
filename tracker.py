b3597b2b750fe645d7a86b8a661ebf35eba1989d
Benedikt Holland <benedikt.holland@protonmail.com>
2024-09-03 18:34:34 +0200

    🧑‍💻 add command line arguments to tracker

--- tracker.py
+++ tracker.py
@@ -1,28 +1,37 @@
 # Check a .md file in your obsidian vault and construct a time series
 import pandas as pd
 from os.path import exists
-import os
 from functions import read_md, to_md
 from dotenv import load_dotenv
+import argparse
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i", "--input-path", type=str, required=True
+    )
+    parser.add_argument(
+        "-o", "--output-path", type=str, required=True
+    )
+    args = parser.parse_args()
     load_dotenv()
-    input_file = os.getenv("DASHBOARD")
-    output_file = os.getenv("HISTORY")
+    input_file = args.input_path
+    output_file = args.output_path
     df = read_md(input_file)
     if exists(output_file):
-        history = pd.read_csv("history.csv", index_col=0)
+        history = pd.read_csv(output_file, index_col=0)
     else:
         history = pd.DataFrame()
     # Lines will be written to history if a column named 'value' contains a value
-    df["max"] = df[["last", "max", "value"]].max(axis=1)
+    if "max" in df.columns and "last" in df.columns:
+        df["max"] = df[["last", "max", "value"]].max(axis=1)
+        df.loc[df.value != "", "last"] = df.loc[df.value != "", "value"]
     done = df[df.value != ""].copy(deep=True)
     done["date"] = pd.Timestamp.today().date()
     done.set_index("date", inplace=True)
     history = pd.concat([history, done])
     history.to_csv(output_file)
     # Reset value
-    df.loc[df.value != "", "last"] = df.loc[df.value != "", "value"]
     df.value = ""
-    to_md(df, os.getenv("DASHBOARD"))
+    to_md(df, input_file)
